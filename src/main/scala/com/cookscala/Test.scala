844dd1c2e788906daa75b6733dfce61adc968824
Bryan Vivanco <bryan.vivanco@bbva.com>
2022-08-23 18:27:49 -0500

    changes

--- src/main/scala/com/cookscala/Test.scala
+++ src/main/scala/com/cookscala/Test.scala
@@ -1,11 +1,5 @@
 package com.cookscala
 
 object Test extends App{
-
-  println("asdasd")
-  println("asdasd")
-  println("asdasd")
-  println("asdasd")
-  println("asdasd")
-  println("12111")
+  println("Nuevo curso Scala")
 }
